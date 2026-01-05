f3d4af783271bb874ac0133c7c1da710a07e9a3f
kwwhs0722 <lhslee0722@gmail.com>
2026-01-05 16:42:48 +0900

    6th commit

--- Day5/arryptr.c
+++ Day5/arryptr.c
@@ -31,9 +31,34 @@
 // }
 
 
-int main(){
-    int ary[3] = {10, 20, 30};
+// int main(){
+//     int ary[3] = {10, 20, 30};
+//     int *pa = ary;
+//     printf("배열의 값: \n");
+//     for(int i=0; i<3; i++){
+//         printf("%5d\n", *pa);
+//         printf("%u\n", pa);
+//         pa++;
+//     }
+//     printf("%u\n", pa);
+
+//     return 0;
+// }
 
 
-    return 0;
-}
+// int main(){
+//     int ary[5] = {10, 20, 30, 40, 50};
+//     int *pa = ary;
+//     int *pb = pa + 3;
+
+//     printf("pa: %u\n", pa);
+//     printf("pb: %u\n", pb);
+//     pa++;
+//     printf("pb - pa = %u\n", pb - pa);
+//     printf("앞에 있는 배열 요소의 값: ");
+//     if(pb > pa) printf("%d\n", *pa);
+//     else printf("%d\n", *pb);
+
+
+//     return 0;
+// }
